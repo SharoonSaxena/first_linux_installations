2524f77ac4773f3868bf3ce8a9e2cb60ecd2abac
Sharoon Saxena <charismaticsharoon@gmail.com>
2022-06-12 15:24:49 +0530

    Update conda-mxnet11.sh

--- conda-mxnet11.sh
+++ conda-mxnet11.sh
@@ -7,4 +7,4 @@ conda install -c conda-forge cudatoolkit=11.2 cudnn=8.1.0
 mkdir -p $CONDA_PREFIX/etc/conda/activate.d
 echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CONDA_PREFIX/lib/' > $CONDA_PREFIX/etc/conda/activate.d/env_vars.sh
 pip install mxnet-cu110==2.0.0a0
-python3 -c "import mxnet as mx: a = mx.nd.ones((2, 3), mx.gpu()): b = a * 2 + 1: b.asnumpy()"
+python3 -c "import mxnet as mx; a = mx.nd.ones((2, 3), mx.gpu()); b = a * 2 + 1; b.asnumpy()"
