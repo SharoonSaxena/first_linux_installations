6b9ae27bfb40a0d9a99dadcaf43d3bd70b23c7a0
Sharoon Saxena <charismaticsharoon@gmail.com>
2022-07-27 23:12:29 +0530

    Update conda-tf-gpu112.sh

--- conda-tf-gpu112.sh
+++ conda-tf-gpu112.sh
@@ -3,9 +3,8 @@ bash Miniconda3-latest-Linux-x86_64.sh
 conda create --name mxnet112 python=3.9.5
 conda activate tf-gpu112
 conda install pip
-conda install -c conda-forge cudatoolkit=11.2 cudnn=8.1.0
+conda install -c conda-forge tensorflow-gpu
 mkdir -p $CONDA_PREFIX/etc/conda/activate.d
 echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CONDA_PREFIX/lib/' > $CONDA_PREFIX/etc/conda/activate.d/env_vars.sh
 sudo echo 0 | sudo tee -a /sys/bus/pci/devices/0000\:01\:00.0/numa_node
-pip install tensorflow
 python3 -c "import tensorflow as tf; print(tf.config.list_physical_devices('GPU'))"
