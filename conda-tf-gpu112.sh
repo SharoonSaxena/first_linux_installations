538db31df80f1f6e3905d9c5a82a80e19c06d67d
Sharoon Saxena <charismaticsharoon@gmail.com>
2022-07-28 22:29:13 +0530

    Update conda-tf-gpu112.sh

--- conda-tf-gpu112.sh
+++ conda-tf-gpu112.sh
@@ -3,7 +3,8 @@ bash Miniconda3-latest-Linux-x86_64.sh
 conda create --name tf-gpu112 python=3.9.5
 conda activate tf-gpu112
 conda install pip
-conda install -c conda-forge tensorflow-gpu
+conda install -c conda-forge cudatoolkit=11.2 cudnn=8.1.0
+pip install tensoflow
 mkdir -p $CONDA_PREFIX/etc/conda/activate.d
 echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CONDA_PREFIX/lib/' > $CONDA_PREFIX/etc/conda/activate.d/env_vars.sh
 ## https://gist.github.com/zrruziev/b93e1292bf2ee39284f834ec7397ee9f
