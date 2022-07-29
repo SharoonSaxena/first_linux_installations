856e7f26ba483679ad79870d57b3549e1b46f893
Sharoon Saxena <charismaticsharoon@gmail.com>
2022-07-29 19:40:04 +0530

    Update conda-tf-gpu112.sh

--- conda-tf-gpu112.sh
+++ conda-tf-gpu112.sh
@@ -11,5 +11,5 @@ pip uninstall PIL
 pip uninstall Pillow
 pip install Pillow scipy pandas matplotlib seaborn
 ## https://gist.github.com/zrruziev/b93e1292bf2ee39284f834ec7397ee9f
-sudo echo 0 | sudo tee -a /sys/bus/pci/devices/0000\:0a\00.0/numa_node
+sudo echo 0 | sudo tee -a /sys/bus/pci/devices/0000\:0a\:00.0/numa_node
 python3 -c "import tensorflow as tf; print(tf.config.list_physical_devices('GPU'))"
